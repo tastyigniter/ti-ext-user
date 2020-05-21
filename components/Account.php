c1c0185a562f964e3b4a1cb64639da2e2a818c69
Sam Poyigi <sam@sampoyigi.com>
2020-05-21 22:25:17 +0100

    Support single character names

--- components/Account.php
+++ components/Account.php
@@ -191,8 +191,8 @@ class Account extends \System\Classes\BaseComponent
             $data = post();
 
             $rules = [
-                ['first_name', 'lang:igniter.user::default.settings.label_first_name', 'required|min:2|max:32'],
-                ['last_name', 'lang:igniter.user::default.settings.label_last_name', 'required|min:2|max:32'],
+                ['first_name', 'lang:igniter.user::default.settings.label_first_name', 'required|min:1|max:48'],
+                ['last_name', 'lang:igniter.user::default.settings.label_last_name', 'required|min:1|max:48'],
                 ['email', 'lang:igniter.user::default.settings.label_email', 'required|email|unique:customers,email'],
                 ['password', 'lang:igniter.user::default.login.label_password', 'required|min:6|max:32|same:password_confirm'],
                 ['password_confirm', 'lang:igniter.user::default.login.label_password_confirm', 'required'],
@@ -253,8 +253,8 @@ class Account extends \System\Classes\BaseComponent
             $data = post();
 
             $rules = [
-                ['first_name', 'lang:igniter.user::default.label_first_name', 'required|min:2|max:32'],
-                ['last_name', 'lang:igniter.user::default.label_last_name', 'required|min:2|max:32'],
+                ['first_name', 'lang:igniter.user::default.label_first_name', 'required|min:1|max:48'],
+                ['last_name', 'lang:igniter.user::default.label_last_name', 'required|min:1|max:48'],
                 ['old_password', 'lang:igniter.user::default.label_email', 'sometimes'],
                 ['new_password', 'lang:igniter.user::default.label_password', 'required_with:old_password|min:6|max:32|same:confirm_new_password'],
                 ['confirm_new_password', 'lang:igniter.user::default.label_password_confirm', 'required_with:old_password'],
