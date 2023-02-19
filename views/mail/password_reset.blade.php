638b1597deabcbb33211a8fc48023cdff5bcc500
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2023-02-19 13:25:24 +0000

    Use blade syntax on all mail templates
    Signed-off-by: Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>

--- views/mail/password_reset.blade.php
+++ views/mail/password_reset.blade.php
@@ -1,19 +1,19 @@
-subject = "Password reset at {site_name}"
+subject = "Password reset at {{$site_name}}"
 ==
-Hi {first_name} {last_name},
+Hi {{$first_name}} {{$last_name}},
 
 Your password was changed successfully!
 
 Please login using your new password:
-{account_login_link}
+{{$account_login_link}}
 
 If you think this password update was a mistake, reset your password immediately.
 ==
-Hi {first_name} {last_name},
+Hi {{$first_name}} {{$last_name}},
 
 ## Your password was changed successfully!
 
-@partial('button', ['url' => '{account_login_link}', 'type' => 'primary'])
+@partial('button', ['url' => $account_login_link, 'type' => 'primary'])
 Login using your new password
 @endpartial
 
