d4937a906d714733b917533a2372b3ae93dd9ba5
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2024-05-17 19:36:57 +0100

    Replace use of deprecated site_url helper method
    Signed-off-by: Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>

--- resources/views/mail/invite_customer.blade.php
+++ resources/views/mail/invite_customer.blade.php
@@ -4,12 +4,12 @@ subject = "Invitation to access {{ $site_name }}"
 
 Click the link below to accept this invitation to gain access to {{ $site_name }}.
 
-{{ site_url('forgot-password?code='.$invite_code) }}
+{{ page_url('account.reset').'?code='.$invite_code }}
 ==
 {{ $full_name }}, you've been invited to access {{ $site_name }}
 
 Accept this invitation to gain access to {{ $site_name }}.
 
-@partial('button', ['url' => site_url('forgot-password?code='.$invite_code), 'type' => 'primary'])
+@partial('button', ['url' => page_url('account.reset').'?code='.$invite_code, 'type' => 'primary'])
 Accept Invitation
 @endpartial
