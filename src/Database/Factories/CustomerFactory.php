359ae3818556160086ca285868a48b3c9f933bd9
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2024-08-10 11:07:55 +0100

    Code refactor
    Signed-off-by: Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>

--- src/Database/Factories/CustomerFactory.php
+++ src/Database/Factories/CustomerFactory.php
@@ -19,7 +19,7 @@ class CustomerFactory extends Factory
             'newsletter' => $this->faker->boolean(),
             'address_id' => $this->faker->numberBetween(1, 9999),
             'customer_group_id' => $this->faker->numberBetween(1, 9999),
-            'status' => '1',
+            'status' => true,
             'is_activated' => $this->faker->boolean(),
             'ip_address' => $this->faker->ipv6(),
         ];
