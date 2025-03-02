tastyigniter/ti-ext-cart

Rename Notification::sendToDatabase method to broadcast

src/Extension.php
@@ -160,7 +160,7 @@ class Extension extends BaseExtension
     protected function bindCheckoutEvents()
     {
         Event::listen('admin.order.paymentProcessed', function (Order $model) {
-            Notifications\OrderCreatedNotification::make()->subject($model)->sendToDatabase();
+            Notifications\OrderCreatedNotification::make()->subject($model)->broadcast();
 
             $model->mailSend('igniter.cart::mail.order', 'customer');
             $model->mailSend('igniter.cart::mail.order_alert', 'location');

tastyigniter/ti-ext-user

src/Extension.php
@@ -22,7 +22,7 @@ class Extension extends \Igniter\System\Classes\BaseExtension
         $this->configureRateLimiting();
 
         Event::listen('igniter.user.register', function (Customer $customer, array $data) {
-            Notifications\CustomerRegisteredNotification::make()->subject($customer)->sendToDatabase();
+            Notifications\CustomerRegisteredNotification::make()->subject($customer)->broadcast();
         });
     }
 