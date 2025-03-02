tastyigniter/ti-ext-cart

Drop activity logger in favour of  laravel notifications.

resources/lang/en/default.php
@@ -174,8 +174,8 @@ return [
         'alert_customer_not_logged' => 'Almost there, Please login or register to complete checkout.',
         'alert_delivery_area_changed' => 'Your delivery area has changed, please confirm the delivery cost.',
 
-        'activity_order_created_title' => 'New order placed',
-        'activity_order_created' => '<b>:properties.full_name</b> created an order.',
+        'notify_order_created_title' => 'New order placed',
+        'notify_order_created' => '<b>%s</b> created an order.',
     ],
 
     'orders' => [

tastyigniter/ti-ext-user

resources/lang/en/default.php
@@ -70,8 +70,8 @@ return [
         'alert_account_activation' => 'An activation email has been sent to your email address.',
         'alert_registration_disabled' => 'Registration is currently disabled by the site administrator.',
 
-        'activity_registered_account_title' => 'Customer registered',
-        'activity_registered_account' => '<b>:properties.full_name</b> created an account.',
+        'notify_registered_account_title' => 'Customer registered',
+        'notify_registered_account' => '<b>%s</b> created an account.',
     ],
 
     'session' => [